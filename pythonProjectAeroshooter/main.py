64d5f92d17622e31cddb68e9b9558ef630cdb16a
Vinícius Pereira dos Santos <viniciuspesan@gmail.com>
2025-03-29 10:51:30 -0300

    initial commit

--- pythonProjectAeroshooter/main.py
+++ pythonProjectAeroshooter/main.py
@@ -8,8 +8,8 @@ print('Setup End')
 print('Loop Start')
 
 while True:
-  # Check for all events
-  for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          pygame.quit()  # Close window
-          quit() # End pygame
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close window
+            quit()  # End pygame
