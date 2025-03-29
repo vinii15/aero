994e6d2f9843cf8b4462849be934ad4f2f18863c
Vinícius Santos <134014274+vinii15@users.noreply.github.com>
2025-03-29 11:12:46 -0300

    Update main.py

--- pythonProjectAeroshooter/main.py
+++ pythonProjectAeroshooter/main.py
@@ -11,5 +11,6 @@ while True:
     # Check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quitting...')
             pygame.quit()  # Close window
             quit()  # End pygame
